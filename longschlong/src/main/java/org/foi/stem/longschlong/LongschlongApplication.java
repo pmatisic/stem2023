53bdd1913a80af37786a899370b2cdf110517efd
Petar <matisicpetar@gmail.com>
2023-05-12 09:58:18 +0200

    Postavljanje projekta

---
+++ longschlong/src/main/java/org/foi/stem/longschlong/LongschlongApplication.java
@@ -0,0 +1,13 @@
+package org.foi.stem.longschlong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongschlongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongschlongApplication.class, args);
+	}
+
+}
---
+++ longschlong/src/test/java/org/foi/stem/longschlong/LongschlongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foi.stem.longschlong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongschlongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
